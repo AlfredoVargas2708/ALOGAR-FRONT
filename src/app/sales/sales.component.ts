86b165c822bc63e1d7d211a480af8b4205af818b
alfredo-vargas <alfvargas@alumnos.uai.cl>
2025-05-26 14:42:24 -0400

    Categories in add product style

--- src/app/sales/sales.component.ts
+++ src/app/sales/sales.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-sales',
